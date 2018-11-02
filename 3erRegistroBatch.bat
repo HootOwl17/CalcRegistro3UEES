78eb169814c2bf3d2be9b6473ecd1c288087cf1d
Dave Morz <davidscout2010@hotmail.com>
2018-11-01 23:51:25 -0600

    Arreglando Codigo... Aún no Funcional >:"v

--- 3erRegistroBatch.bat
+++ 3erRegistroBatch.bat
@@ -2,8 +2,8 @@
 title Programa para Calcular promedio para 3er Registro
 set /p num1 = Ingrese Nota de 1er Registro
 set /p num2 = Ingrese Nota de 2do Registro
-set /a suma = 6 - ((%num1% + %num2%)*(3/10))
-set /a promedio = %suma%/(4/10)
+set /a suma = 6 - ((%num1%+%num2%)*(3/10))
+set /a promedio=(%suma% * (4/10))
 echo
 echo Necesita una nota de %promedio%
 pause >nul
