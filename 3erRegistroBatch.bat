f921cd1ac79d4d3d9f3a2fac0f7c104228caed15
HootOwl17 <davidscout2010@hotmail.com>
2022-09-13 11:02:32 -0600

    Update 3erRegistroBatch.bat
    Agrega descripción para evitar problemas de mal ingreso de datos.

--- 3erRegistroBatch.bat
+++ 3erRegistroBatch.bat
@@ -1,7 +1,7 @@
 @echo off
 title Calcular Nota para 3er Registro UEES
-set/p "num1=Cual es la nota de 1er Registro?   "
-set/p "num2=Cual es la nota de 2do Registro?   "
+set/p "num1=Cual es la nota de 1er Registro?   (Nota sin decimales Ej. 7.5 = 750)"
+set/p "num2=Cual es la nota de 2do Registro?   (Nota sin decimales Ej. 7.5 = 750)"
 set/a media= ((%num1% + %num2%) * 3)/10)
 set/a resta = 600 - %media%
 set/a prom = (%resta% * 10)/(4)
