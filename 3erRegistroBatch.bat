692d249bec62bc4aff3ab2c2bfcc7e1bd9852da5
Dave Morz <35872932+HootOwl17@users.noreply.github.com>
2023-11-15 11:24:28 -0600

    Update 3erRegistroBatch.bat

--- 3erRegistroBatch.bat
+++ 3erRegistroBatch.bat
@@ -1,8 +1,10 @@
 @echo off
 title Calcular Nota para 3er Registro UEES
-set/p "num1=Cual es la nota de 1er Registro?   (Nota sin decimales Ej. 7.5 = 750)"
-set/p "num2=Cual es la nota de 2do Registro?   (Nota sin decimales Ej. 7.5 = 750)"
-set/a media= ((%num1% + %num2%) * 3)/10)
+echo Cual es la nota de 1er Registro?   (Nota sin decimales Ej. 7.5 = 750)
+set/p "num1="
+echo Cual es la nota de 2do Registro?   (Nota sin decimales Ej. 7.5 = 750)
+set/p "num2="
+set/a media = ((%num1% + %num2%) * 3)/10)
 set/a resta = 600 - %media%
 set/a prom = (%resta% * 10)/(4)
 echo.
